73312615ade08edf69f80975176b027cfd6dbb6f
Katsumasa Yakushiji <yakuadosoft@live.jp>
2019-03-04 22:51:50 +0900

    Add Stream Result Extention

---
+++ CSharpFunctional/Extention/StreamResultExtention.cs
@@ -0,0 +1,67 @@
+﻿using CSharpFunctional.Structure;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CSharpFunctional.Extention
+{
+    /// <summary>
+    /// Extention Run Return Result Function with Stream  
+    /// </summary>
+    public static class StreamResultExtention
+    {
+        /// <summary>
+        /// Combine acquisition, execution, and conversion
+        /// </summary>
+        /// <typeparam name="S">type of Stream</typeparam>
+        /// <typeparam name="T">in Success Type</typeparam>
+        /// <typeparam name="ET">in Error Type</typeparam>
+        /// <param name="returnStreamFunc">Return Stream Function</param>
+        /// <param name="exceptionConverter">Exception to in Error Type Converter</param>
+        /// <param name="useStreamFunc">Function with Stream</param>
+        /// <returns></returns>
+        public static Result<T,ET> Run<S, T, ET>(this Func<S> returnStreamFunc, Func<Exception, ET> exceptionConverter, 
+            Func<S, Result<T, ET>> useStreamFunc) where S : Stream
+        {
+            try
+            {
+                using (var stream = returnStreamFunc())
+                {
+                    return useStreamFunc(stream);
+                }
+            }
+            catch(Exception ex)
+            {
+                return Result<T, ET>.Error(exceptionConverter(ex));
+            }
+        }
+
+        /// <summary>
+        /// Combine acquisition, execution, and conversion
+        /// </summary>
+        /// <typeparam name="S">type of Stream</typeparam>
+        /// <typeparam name="T">in Success Type</typeparam>
+        /// <typeparam name="ET">in Error Type</typeparam>
+        /// <param name="returnStreamFunc">Return Stream Function</param>
+        /// <param name="exceptionConverter">Exception to in Error Type Converter</param>
+        /// <param name="useStreamAction">Action with Stream</param>
+        /// <returns></returns>
+        public static Result<T, ET> Run<S, T, ET>(this Func<S> returnStreamFunc, Func<Exception, ET> exceptionConverter,
+            Action<S> useStreamAction) where S : Stream
+        {
+            try
+            {
+                using (var stream = returnStreamFunc())
+                {
+                    useStreamAction(stream);
+                    return Result<T, ET>.Success(default(T));
+                }
+            }
+            catch (Exception ex)
+            {
+                return Result<T, ET>.Error(exceptionConverter(ex));
+            }
+        }
+    }
+}
---
+++ CSharpFunctionalTest/Extention/StreamResultExtentionTest.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using CSharpFunctional.Extention;
+using CSharpFunctional.Structure;
+
+namespace CSharpFunctionalTest.Extention
+{
+    [TestClass]
+    public class StreamResultExtentionTest
+    {
+
+        private const int testCapacity = 1024;
+        private readonly byte[] overCapacityBuffer = new byte[testCapacity + 1];
+        private readonly Func<MemoryStream> testStreamFunc = GetTestStream;
+        private const string notMatch = "Not Match";
+
+        [TestCategory("CSharpFunctional_Extention_StreamResultExtention")]
+        [TestMethod]
+        public void StreamResultTest()
+        {
+            {
+                var result = testStreamFunc.Run(ExceptionToString, m =>
+                {
+                    if (m.Capacity == testCapacity)
+                    {
+                        return Result<bool, string>.Success(true);
+                    }
+                    else
+                    {
+                        return Result<bool, string>.Error("Not Match");
+                    }
+                });
+                Assert.IsTrue(result.Value);
+                Assert.IsTrue(result.IsOK);
+                Assert.IsFalse(result.IsError);
+            }
+
+            {
+                var result = testStreamFunc.Run(ExceptionToString, m =>
+                {
+                    if (m.Capacity == 0)
+                    {
+                        return Result<bool, string>.Success(true);
+                    }
+                    else
+                    {
+                        return Result<bool, string>.Error(notMatch);
+                    }
+                });
+                Assert.IsFalse(result.IsOK);
+                Assert.IsTrue(result.IsError);
+                Assert.AreEqual(result.ErrorValue, notMatch);
+            }
+
+            {
+                var result = testStreamFunc.Run<MemoryStream, bool ,string>(ExceptionToString, m =>
+                {
+                    throw new ArgumentOutOfRangeException();
+                });
+                Assert.IsFalse(result.IsOK);
+                Assert.IsTrue(result.IsError);
+                Assert.AreEqual(result.ErrorValue, typeof(ArgumentOutOfRangeException).ToString());
+            }
+        }
+
+        private static MemoryStream GetTestStream()
+        {
+            return new MemoryStream(testCapacity);
+        }
+
+        private static string ExceptionToString(Exception ex)
+        {
+            return ex.GetType().ToString();
+        }
+    }
+}
